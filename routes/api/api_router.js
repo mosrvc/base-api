de86a8f2546e8f4cb95451551a97d16039c630cd
Jimmy Richard Timotius Dumalang <jimmy.dumalang@adira.co.id>
2022-04-05 14:43:40 +0700

    Fix wrong route

--- routes/api/api_router.js
+++ routes/api/api_router.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const userRoutes = require('./users')
+const userRoutes = require('./user_router')
 
 /* GET users listing. */
 router.use('/users', userRoutes);
